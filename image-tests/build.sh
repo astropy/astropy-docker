0781ce68bdb365690550eab3a5549e5565eea534
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-10-29 10:23:55 +0000

    Tagged version 1.7 of images, which contain latest version of pytest-astropy and dependencies

---
+++ image-tests/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script to build the latest active versions of the MPL images
+
+cd py36-mpl212
+docker build --no-cache . -t astropy/image-tests-py36-mpl212:1.7
+cd ..
+
+cd py36-mpl222
+docker build --no-cache . -t astropy/image-tests-py36-mpl222:1.7
+cd ..
+
+cd py37-mpl302
+docker build --no-cache . -t astropy/image-tests-py37-mpl302:1.7
+cd ..
+
+cd py37-mpl311
+docker build --no-cache . -t astropy/image-tests-py37-mpl311:1.7
+cd ..
+
+docker push astropy/image-tests-py36-mpl212:1.7
+docker push astropy/image-tests-py36-mpl222:1.7
+docker push astropy/image-tests-py37-mpl302:1.7
+docker push astropy/image-tests-py37-mpl311:1.7
