d9577be0f3e773316db2f3a3302273fed9b944f1
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-01-16 10:50:51 +0000

    Re-build mpl images with latest version of pytest-astropy (1.10)

--- image-tests/build.sh
+++ image-tests/build.sh
@@ -3,22 +3,22 @@
 # Script to build the latest active versions of the MPL images
 
 cd py36-mpl212
-docker build --no-cache . -t astropy/image-tests-py36-mpl212:1.9
+docker build --no-cache . -t astropy/image-tests-py36-mpl212:1.10
 cd ..
 
 cd py36-mpl222
-docker build --no-cache . -t astropy/image-tests-py36-mpl222:1.9
+docker build --no-cache . -t astropy/image-tests-py36-mpl222:1.10
 cd ..
 
 cd py37-mpl302
-docker build --no-cache . -t astropy/image-tests-py37-mpl302:1.9
+docker build --no-cache . -t astropy/image-tests-py37-mpl302:1.10
 cd ..
 
 cd py37-mpl311
-docker build --no-cache . -t astropy/image-tests-py37-mpl311:1.9
+docker build --no-cache . -t astropy/image-tests-py37-mpl311:1.10
 cd ..
 
-docker push astropy/image-tests-py36-mpl212:1.9
-docker push astropy/image-tests-py36-mpl222:1.9
-docker push astropy/image-tests-py37-mpl302:1.9
-docker push astropy/image-tests-py37-mpl311:1.9
+docker push astropy/image-tests-py36-mpl212:1.10
+docker push astropy/image-tests-py36-mpl222:1.10
+docker push astropy/image-tests-py37-mpl302:1.10
+docker push astropy/image-tests-py37-mpl311:1.10
